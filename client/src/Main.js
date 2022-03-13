1bbe8b84d75f0cc567b4152e8471f845e46bccfd
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2022-03-14 03:40:49 +0530

    add c++ algo

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import bars from './img/bars.svg'
-import search from './img/search.svg'
-import './App.css';
+import logo from "./logo.svg";
+import bars from "./img/bars.svg";
+import search from "./img/search.svg";
+import "./App.css";
 import GridTable from "@nadavshaar/react-grid-table";
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from "react";
 import getColumns from "./getColumns.js";
-import axios from 'axios';
-import moment from 'moment';
+import axios from "axios";
+import moment from "moment";
+import code from "./code.txt";
+import input from "./input.txt";
 
 // const data = [
 //   {
@@ -76,68 +78,212 @@ import moment from 'moment';
 //     acceptedDate: "2/01/2022",
 //   },
 // ]
-
+let algo1 = "1";
+let input1 = "1";
+let data = [];
 function Main() {
-    const [clicked,setClicked] = useState(false); 
-    const [redClicked,setRedClicked] = useState(false);
-    const [greenClicked,setGreenClicked] = useState(false);
-    const [rowsData, setRowsData] = useState([]);
-    const [isLoading, setLoading] = useState(false);
-    useEffect(() => {
-      setLoading(true);
-      // setRowsData(data);
-      axios.get('http://localhost:4000/api/cases/getcase').then(res => {
+  const [clicked, setClicked] = useState(false);
+  const [redClicked, setRedClicked] = useState(false);
+  const [greenClicked, setGreenClicked] = useState(false);
+  const [rowsData, setRowsData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    console.log(code);
+    // setRowsData(data);
+
+    fetch(code)
+      .then((r) => r.text())
+      .then((text) => (algo1 = text));
+    fetch(input)
+      .then((r) => r.text())
+      .then((text) => {
+        input1 = text;
+        submit();
+      });
+
+    axios
+      .get("http://localhost:4000/api/cases/getcase")
+      .then((res) => {
         // console.log(res.data)
-        let data = []
-        res.data.data.map((item,idx) => {
-         let sections=""
-         item.section.map(temp => {
-           if(sections.length===0) sections+=temp.name 
-           else sections=sections+","+temp.name
-         })
-         data.push({
-           id:idx,
-           caseId:item.case_id,
-           domain:item.domain,
-           sections,
-           accusedStatus:item.accusedStatus===0?"On Bail" :item.accusedStatus===1? "On Run" : "In Jail" ,
-           lastHearingDate:moment(item.lastDate).format("YYYY-MM-DD"),
-           proposedDate: "2022-02-01",
-           acceptedDate: item.nextHearingDate ? moment(item.nextHearingDate).format("YYYY-MM-DD") :"Select a Date",
 
-         })
-        })
-        setRowsData(data)
-      }).catch(error => {
-        console.log(error)
+        res.data.data.map((item, idx) => {
+          let sections = "";
+          item.section.map((temp) => {
+            if (sections.length === 0) sections += temp.name;
+            else sections = sections + "," + temp.name;
+          });
+          data.push({
+            id: idx,
+            caseId: item.case_id,
+            domain: item.domain,
+            sections,
+            accusedStatus:
+              item.accusedStatus === 0
+                ? "On Bail"
+                : item.accusedStatus === 1
+                ? "On Run"
+                : "In Jail",
+            lastHearingDate: moment(item.lastDate).format("YYYY-MM-DD"),
+            proposedDate: "2022-02-01",
+            acceptedDate: item.nextHearingDate
+              ? moment(item.nextHearingDate).format("YYYY-MM-DD")
+              : "Select a Date",
+          });
+        });
+        setRowsData(data);
       })
-    }, []);
-    
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    
+  const submit = async () => {
+    const response = await fetch(
+      "https://judge0-ce.p.rapidapi.com/submissions",
+      {
+        method: "POST",
+        headers: {
+          "x-rapidapi-host": process.env.REACT_APP_RAPID_HOST,
+          "x-rapidapi-key": process.env.REACT_APP_RAPID_KEY,
+          "content-type": "application/json",
+          accept: "application/json",
+        },
+        body: JSON.stringify({
+          source_code: algo1,
+          stdin: input1,
+          language_id: 54,
+        }),
+      }
+    );
+    const jsonResponse = await response.json();
+    console.log(jsonResponse + "jsonres");
+    let jsonGetSolution = {
+      status: {
+        description: "Queue",
+      },
+      stderr: null,
+      compile_output: null,
+    };
+
+    while (
+      jsonGetSolution.status.description !== "Accepted" &&
+      jsonGetSolution.stderr == null &&
+      jsonGetSolution.compile_output == null
+    ) {
+      if (jsonResponse.token) {
+        let url = `https://judge0-ce.p.rapidapi.com/submissions/${jsonResponse.token}?base64_encoded=true`;
+
+        const getSolution = await fetch(url, {
+          method: "GET",
+          headers: {
+            "x-rapidapi-key": process.env.REACT_APP_RAPID_KEY,
+            "x-rapidapi-host": process.env.REACT_APP_RAPID_HOST,
+            "content-type": "application/json",
+          },
+        });
+
+        jsonGetSolution = await getSolution.json();
+      }
+    }
+
+    if (jsonGetSolution.stdout) {
+      const output = atob(jsonGetSolution.stdout);
+      console.log(output);
+    } else if (jsonGetSolution.stderr) {
+      const error = atob(jsonGetSolution.stderr);
+      console.log(error + "err");
+    } else {
+      const compilation_error = atob(jsonGetSolution.compile_output);
+    }
+  };
+
   return (
     <div className="Main">
       <div className="search-bar">
-          <div className="d-flex search-bar-box">
-              <div><img src={bars} style={{width:"25px",margin:"5px 10px"}}></img></div>
-              <input style={{width:"95%"}} className="search-text" value="Type Case ID here"></input>
-              <div><img src={search} style={{width:"25px",margin:"5px 10px"}}></img></div>
+        <div className="d-flex search-bar-box">
+          <div>
+            <img src={bars} style={{ width: "25px", margin: "5px 10px" }}></img>
           </div>
+          <input
+            style={{ width: "95%" }}
+            className="search-text"
+            value="Type Case ID here"
+          ></input>
+          <div>
+            <img
+              src={search}
+              style={{ width: "25px", margin: "5px 10px" }}
+            ></img>
+          </div>
+        </div>
       </div>
-      <div className="widget-container"> 
-      <div className="d-flex widget-box">
-              <div className="widget" style={{marginRight:"180px",marginLeft:"15px", border: "3px solid #FFA004",backgroundColor:clicked?"#FFA004":"white",color:clicked?"white":"black",cursor:"pointer"}} onClick={() => setClicked(!clicked)}> 
-              <p style={{textAlign:"center",margin:"10px 0px"}}><span style={{fontSize:"76px"}}>91</span><br></br><span>hearings today</span></p></div>
-              <div className="widget" style={{marginRight:"180px", border: "3px solid #23CA34",backgroundColor:greenClicked?"#23CA34":"white",color:greenClicked?"white":"black",cursor:"pointer"}} onClick={() => setGreenClicked(!greenClicked)}>
-              <p style={{textAlign:"center",margin:"10px 0px"}}><span style={{fontSize:"76px"}}>11</span><br></br><span>cases need validation</span></p>
-              </div>
-              <div className="widget" style={{border: "3px solid #EC2E2E",backgroundColor:redClicked?"#EC2E2E":"white",color:redClicked?"white":"black",cursor:"pointer"}} onClick={() => setRedClicked(!redClicked)}>
-              <p style={{textAlign:"center",margin:"10px 0px"}}><span style={{fontSize:"76px"}}>1103</span><br></br><span>cases has priority high but next proposed hearing date {'>'} 1month</span></p>
-              </div>
+      <div className="widget-container">
+        <div className="d-flex widget-box">
+          <div
+            className="widget"
+            style={{
+              marginRight: "180px",
+              marginLeft: "15px",
+              border: "3px solid #FFA004",
+              backgroundColor: clicked ? "#FFA004" : "white",
+              color: clicked ? "white" : "black",
+              cursor: "pointer",
+            }}
+            onClick={() => setClicked(!clicked)}
+          >
+            <p style={{ textAlign: "center", margin: "10px 0px" }}>
+              <span style={{ fontSize: "76px" }}>91</span>
+              <br></br>
+              <span>hearings today</span>
+            </p>
           </div>
+          <div
+            className="widget"
+            style={{
+              marginRight: "180px",
+              border: "3px solid #23CA34",
+              backgroundColor: greenClicked ? "#23CA34" : "white",
+              color: greenClicked ? "white" : "black",
+              cursor: "pointer",
+            }}
+            onClick={() => setGreenClicked(!greenClicked)}
+          >
+            <p style={{ textAlign: "center", margin: "10px 0px" }}>
+              <span style={{ fontSize: "76px" }}>11</span>
+              <br></br>
+              <span>cases need validation</span>
+            </p>
+          </div>
+          <div
+            className="widget"
+            style={{
+              border: "3px solid #EC2E2E",
+              backgroundColor: redClicked ? "#EC2E2E" : "white",
+              color: redClicked ? "white" : "black",
+              cursor: "pointer",
+            }}
+            onClick={() => setRedClicked(!redClicked)}
+          >
+            <p style={{ textAlign: "center", margin: "10px 0px" }}>
+              <span style={{ fontSize: "76px" }}>1103</span>
+              <br></br>
+              <span>
+                cases has priority high but next proposed hearing date {">"}{" "}
+                1month
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
       <div className="react-table">
-      <GridTable columns={getColumns({ setRowsData })} rows={rowsData} isPaginated={false} isLoading={isLoading} />
+        <GridTable
+          columns={getColumns({ setRowsData })}
+          rows={rowsData}
+          isPaginated={false}
+          isLoading={isLoading}
+        />
       </div>
     </div>
   );
--- server/api/cases.js
+++ server/api/cases.js
@@ -4,16 +4,22 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const CaseInfo = require("../models/caseInfo");
 // const Answer = require("../db/Answer");
+var mp = new Map([
+  [3, 2],
+  [1, 2],
+]);
 router.post("/newCase", async (req, res) => {
   try {
     const newCase = new CaseInfo();
     console.log(req.body);
+    console.log(mp.get(3));
     newCase.case_id = req.body.case_id;
     newCase.domain = req.body.domain;
     newCase.section = req.body.section;
     newCase.prevDates = req.body.prevDates;
     newCase.lastDate = req.body.lastDate;
     newCase.accusedStatus = req.body.accusedStatus;
+    newCase.nextHearingDate = req.body.nextHearingDate;
 
     let newCaseModel = new CaseInfo(newCase);
     await newCaseModel.save();
