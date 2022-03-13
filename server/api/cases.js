adfa62c0405021b6ad9efb30b79309adbfdf37c5
Abhay Raj <abhayrajsingh1707@gmail.com>
2022-03-13 21:51:13 +0530

    adding api for getting all the data

--- server/api/cases.js
+++ server/api/cases.js
@@ -23,4 +23,21 @@ router.post("/newCase", async (req, res) => {
   }
 });
 
+// for getting all cases
+router.get("/getCase", async (req, res) => {
+  try {
+    const caseInfo = await CaseInfo.find({});
+    console.log(caseInfo);
+    res.status(200).send({data: caseInfo});
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({error: error});
+  
+  }
+  });
+
+  // for updating all the cases
+
+
+
 module.exports = router;
