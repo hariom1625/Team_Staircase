c0c3be46450d77a6808b5b4078a13974943d6d43
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2022-08-26 09:34:40 +0530

    update logging

--- src/server/api/user.js
+++ src/server/api/user.js
@@ -7,12 +7,23 @@ const LogInfo = require("../models/logs");
 
 router.post("/login", async (req, res) => {
   const newLog = new LogInfo();
-  newLog.userId = req.body.userId;
+  newLog.userId = req.body.userName;
   newLog.apiEndPoint = "/login";
   newLog.ipAddress = req.body.ipAddress;
+  newLog.statement = "User Succesfully loggedIn";
   let newLogModel = new LogInfo(newLog);
   await newLogModel.save();
-  res.send("Login Logged");
+  res.send("User loggedIn");
+});
+router.post("/incorrectCredentials", async (req, res) => {
+  const newLog = new LogInfo();
+  newLog.userId = req.body.userName;
+  newLog.apiEndPoint = "/login";
+  newLog.ipAddress = req.body.ipAddress;
+  newLog.statement = "Unauthorized access attempted";
+  let newLogModel = new LogInfo(newLog);
+  await newLogModel.save();
+  res.send("Unauthorized access attempted");
 });
 
 module.exports = router;
